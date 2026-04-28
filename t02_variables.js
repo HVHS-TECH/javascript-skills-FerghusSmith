cf19bf8eee805f670dbe3fdc606bbaf8096f7b28
FerghusSmith <24311fs@hvhs.school.nz>
2026-04-28 23:52:18 +0000

    Apr 28, 2026, 11:52 PM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,18 @@
-/* Paste your code from task 1 here */
+/*
+Name of task: intro
+*/
+console.log("Running t01_introduction.js");
+
+//Variables
+let name = "me";
+
+
+/*
+Main Code
+*/
+console.log("Hello World");
+console.log("you are " + name);
+/*
+Functions
+*/
+
