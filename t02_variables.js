a654b8710b579898bf7237c03387e0a96ad4078f
FerghusSmith <24311fs@hvhs.school.nz>
2026-04-29 00:14:02 +0000

    Apr 29, 2026, 12:14 AM

--- t02_variables.js
+++ t02_variables.js
@@ -4,13 +4,20 @@ Name of task: variables
 console.log("Running t01_introduction.js");
 
 //Variables
-let name = "jeff"
-let money = "20";
-let age = "12";
-let time = "now";
+let name;
+let money;
+let age;
+let time;
 /*
 Main Code
 */
+
+name = window.prompt("what is your name?");
+money = window.prompt("how much money do you have?");
+age = window.prompt("How old are you");
+time = window.prompt("What is the date?");
+
+
 console.log("hi " + name);
 console.log("you have $" + money + " dollars");
 console.log("as of " + time + " you are " + age + " years old");
