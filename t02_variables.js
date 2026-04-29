90fd016441b5d1a0b7f8bdfc60178a7a9197b71e
FerghusSmith <24311fs@hvhs.school.nz>
2026-04-29 00:07:42 +0000

    Apr 29, 2026, 12:07 AM

--- t02_variables.js
+++ t02_variables.js
@@ -4,17 +4,31 @@ Name of task: variables
 console.log("Running t01_introduction.js");
 
 //Variables
-let name = "jeff";
 let money = "20";
 let age = "12";
 let time = "now";
 /*
 Main Code
 */
-console.log("hi " + name);
+console.log("hi ");
 console.log("you have $" + money + " dollars");
 console.log("as of " + time + " you are " + age + " years old");
 /*
 Functions
 */
+
+
+const prompt = require('prompt-sync')();
+const name = prompt('What is your name? ');
+
+javascript
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('What is your name? ', name => {
+  console.log(`Hello ${name}!`);
+  readline.close();
+});
 
