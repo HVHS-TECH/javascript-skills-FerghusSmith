f31600482346aa20db836cff2407e56f649e0115
FerghusSmith <24311fs@hvhs.school.nz>
2026-04-30 00:37:16 +0000

    Apr 30, 2026, 12:37 AM

--- t02_variables.js
+++ t02_variables.js
@@ -15,12 +15,13 @@ Main Code
 name = window.prompt("what is your name?");
 money = window.prompt("how much money do you have?");
 age = parseInt(window.prompt("How old are you"));
-time = window.prompt("What is the date?");
+time = window.prompt("What is the year?");
 
 
 console.log("hi " + name);
 console.log("you have " + money + " dollars");
-console.log("as of " + time + " you are " + age + " years old");
+console.log("as of the year " + time + " you are " + age + " years old");
+console.log("You were born in " + (t))
 console.log("in 10 years you will be " + (age + 10) + " years old");
 console.log("you were " + (age / 2) + " hald your life ago")
 /*
