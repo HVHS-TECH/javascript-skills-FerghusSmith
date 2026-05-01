00d31625adedb9067a79b15ca61772bc46466721
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-01 02:49:06 +0000

    May 1, 2026, 2:49 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,36 @@
-/* Paste your code from task 3, Maths here */
+/*
+Name of task: variables
+*/
+console.log("Running t01_introduction.js");
+
+//Variables
+let name;
+let money;
+let age;
+let time;
+/*
+Main Code
+*/
+const OUTPUT = document.getElementById("JavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added By Java Script</h2>"
+name = window.prompt("what is your name?");
+money = parseInt(window.prompt("how much money do you have?"));
+age = parseInt(window.prompt("How old are you"));
+time = parseInt(window.prompt("What is the year?"));
+
+
+console.log("hi " + name);
+console.log("you have " + money + " dollars");
+console.log("as of the year " + time + " you are " + age + " years old");
+console.log("You were born in " + (time - age));
+console.log("In 10 years you will be " + (age + 10) + " years old");
+console.log("You will be 50 in the year " + ((50 - age) + time));
+console.log("You will die on average in the year " + ((time - age) + 73));
+console.log("You will die in " + ((73 - age)) + " years");
+console.log("If you spend half your money, you wil have " + (money / 2));
+console.log("If you won the average lottery, you will have " + (money + 350000)); 
+console.log("If you died right now, there would be a %" + ((age - 15) / 0.7) + " chance that you would have passed on your genes");
+console.log("If you die in 10 years, there would be a %" + ((age + 5) / 0.7) + " chance that you would have passed on your genes");
+/*
+Functions
+*/
