b9fd2606ad3701dc2656b200e3cb6a2ac4cc6e1f
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-01 02:44:53 +0000

    May 1, 2026, 2:44 AM

--- t04_debugging.js
+++ t04_debugging.js
@@ -3,7 +3,7 @@ console.log("Task 4 - Debugging");
 // calculate the area
 var length = 8;
 var width = 10
-var area = length + width;
+var area = length * width;
 
 // calculate the perimeter
 var perimeter = (2 * length) + (2 * width); 
@@ -15,8 +15,8 @@ console.log("A area of a piece of paper with a length of " + length + ", and a w
 console.log("The same piece of paper has a perimeter of "+ perimeter);
 
 // Check, does this result look sensible
-if (perimeter > area){
+if (perimeter < area){
     console.log("Yay, your area is larger, this is right for this example")
 }else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example);
+    console.log("Hmm, your perimeter is larger. It shouldn't be for this example");
 }
