76949ff818945aba66d8e0a8d6dd77dd537299b1
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-04 21:43:10 +0000

    May 4, 2026, 9:43 PM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -1,1 +1,23 @@
-/* Paste your code from task 6 here */
+/*
+Name of task: functions with parameters
+*/
+console.log("Running t07_functions_parameters.js");
+
+//Variables
+
+/*
+Main Code
+*/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+OUTPUT.innerHTML = "<h2>Added By Java Script</h2>";
+displayProduct("Chocolate bar", 4);
+
+/*
+Functions
+*/
+function welcome(){
+    OUTPUT.innerHTML += "<p>Welcome to the shop</p>"
+}
+function displayProduct(_name, _price){
+    OUTPUT.innerHTML += "<p>" + _name + ": $" + _price "</p>";
+}
