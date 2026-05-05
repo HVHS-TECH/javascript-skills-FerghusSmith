3393627d4d179011fa46faaa776b90f0174bba96
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-05 22:07:18 +0000

    May 5, 2026, 10:07 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,30 @@
-/* Paste your code from the last task */
+/*
+Name of task: functions with parameters
+*/
+console.log("Running t07_functions_parameters.js");
+
+//Variables
+
+/*
+Main Code
+*/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+
+/*
+Functions
+*/
+function welcome(){
+    OUTPUT.innerHTML = "<p>Welcome to the shop</p>"
+}
+function displayProduct(_name, _price){
+    OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>";
+}
+function start(){
+    welcome();
+    displayProduct("Chocolate bar", 4);
+    displayProduct("Chips", 3);
+    displayProduct("drink", 2.5);
+    displayProduct("Famous brownie slice", 5);
+    displayProduct("Famous brownie batch", 100);
+}
