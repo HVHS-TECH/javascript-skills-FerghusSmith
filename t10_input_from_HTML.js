e640e9b458ca75fadcd57ba45ba6495261c29d02
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-05 22:17:54 +0000

    May 5, 2026, 10:17 PM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -28,3 +28,8 @@ function start(){
     displayProduct("Famous brownie slice", 5);
     displayProduct("Famous brownie batch", 100);
 }
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML += "<p>Your name is "+userName+"</p>";
+}
