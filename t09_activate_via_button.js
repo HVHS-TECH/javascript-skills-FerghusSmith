e59327f24eda1591ee631154b2e2aa824c755a1a
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-05 22:05:51 +0000

    May 5, 2026, 10:05 PM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -9,22 +9,22 @@ console.log("Running t07_functions_parameters.js");
 Main Code
 */
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-welcome();
-displayProduct("Chocolate bar", 4);
-displayProduct("Chips", 3);
-displayProduct("drink", 2.5);
-displayProduct("Famous brownie slice", 5);
-displayProduct("Famous brownie batch", 100);
+
 
 /*
 Functions
 */
 function welcome(){
-OUTPUT.innerHTML = "<p>Welcome to the shop</p>"
+    OUTPUT.innerHTML = "<p>Welcome to the shop</p>"
 }
 function displayProduct(_name, _price){
-OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>";
+    OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>";
 }
 function start(){
-    spaceForJavaScriptOutput.innerHTML
+    welcome();
+    displayProduct("Chocolate bar", 4);
+    displayProduct("Chips", 3);
+    displayProduct("drink", 2.5);
+    displayProduct("Famous brownie slice", 5);
+    displayProduct("Famous brownie batch", 100);
 }
