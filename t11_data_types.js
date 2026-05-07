8d972865a8e1912c49f6ef1e724b6e0dcb24a4ea
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-07 01:16:40 +0000

    May 7, 2026, 1:16 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -14,8 +14,15 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 /*
 Functions
 */
-function getFormInput(){
+function getFormInputAge(){
     const AGE_FIELD = document.getElementById("ageField");
-    let userAge = number(AGE_FIELD.value);
-    OUTPUT.innerHTML = "<p>Your age is "+userAge+"</p>";
+    let userAge = Number(AGE_FIELD.value);
+    OUTPUT.innerHTML += "<p>Your age is "+userAge+"</p>";
+    OUTPUT.innerHTML += "<p>In 1 year your age will be "+(userAge+1)+"</p>"
+}
+function getFormInputMoney(){
+    const AGE_FIELD = document.getElementById("moneyField");
+    let userMoney = Number(AGE_FIELD.value);
+    OUTPUT.innerHTML += "<p>You have $"+userMoney+"</p>";
+    OUTPUT.innerHTML += "<p>If you gain $1 you will have $"+(userMoney+1)+"</p>"
 }
