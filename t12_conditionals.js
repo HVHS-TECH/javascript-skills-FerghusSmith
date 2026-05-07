37fef49ab29eec54a19a22b9ee3f4ffc7258f5c8
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-07 01:24:26 +0000

    May 7, 2026, 1:24 AM

--- t12_conditionals.js
+++ t12_conditionals.js
@@ -1,1 +1,28 @@
-/* Paste your code from the last task */
+/*
+Name of task: functions with parameters
+*/
+console.log("Running t07_functions_parameters.js");
+
+//Variables
+
+/*
+Main Code
+*/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+
+/*
+Functions
+*/
+function getFormInputAge(){
+    const AGE_FIELD = document.getElementById("ageField");
+    let userAge = Number(AGE_FIELD.value);
+    OUTPUT.innerHTML += "<p>Your age is "+userAge+"</p>";
+    OUTPUT.innerHTML += "<p>In 1 year your age will be "+(userAge+1)+"</p>"
+}
+function getFormInputMoney(){
+    const AGE_FIELD = document.getElementById("moneyField");
+    let userMoney = Number(AGE_FIELD.value);
+    OUTPUT.innerHTML += "<p>You have $"+userMoney+"</p>";
+    OUTPUT.innerHTML += "<p>If you gain $1 you will have $"+(userMoney+1)+"</p>"
+}
