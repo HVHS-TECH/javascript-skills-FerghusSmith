cd09216a1c05a6f347400e2f30306332cf681e6e
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-07 01:04:48 +0000

    May 7, 2026, 1:04 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -15,7 +15,7 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 Functions
 */
 function getFormInput(){
-    const NAME_FIELD = document.getElementById("nameField");
-    let userName = NAME_FIELD.value;
-    OUTPUT.innerHTML += "<p>Your name is "+userName+"</p>";
+    const AGE_FIELD = document.getElementById("nameField");
+    let userAge = number(AGE_FIELD.value);
+    OUTPUT.innerHTML = "<p>Your age is "+userAge+"</p>"
 }
