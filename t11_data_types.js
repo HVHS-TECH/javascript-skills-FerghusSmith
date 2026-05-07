a4b58c7ddc0e645d0772d7ce73ad0d777fb9419f
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-07 01:01:21 +0000

    May 7, 2026, 1:01 AM

--- t11_data_types.js
+++ t11_data_types.js
@@ -1,1 +1,21 @@
-/* Paste your code from the last task */
+/*
+Name of task: functions with parameters
+*/
+console.log("Running t07_functions_parameters.js");
+
+//Variables
+
+/*
+Main Code
+*/
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+
+/*
+Functions
+*/
+function getFormInput(){
+    const NAME_FIELD = document.getElementById("nameField");
+    let userName = NAME_FIELD.value;
+    OUTPUT.innerHTML += "<p>Your name is "+userName+"</p>";
+}
