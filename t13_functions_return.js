3390eec8502e68819ae764a5b46e1dc88329e2d4
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-08 03:00:24 +0000

    May 8, 2026, 3:00 AM

--- t13_functions_return.js
+++ t13_functions_return.js
@@ -8,26 +8,30 @@ console.log("Running t07_functions_parameters.js");
 /*
 Main Code
 */
-const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 const OUTPUT_START = document.getElementById("spaceForJavaScriptOutputStart");
-
+const OUTPUT_ITEMS = document.getElementById("spaceForJavaScriptOutputStart");
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 /*
 Functions
 */
 function start(){
     OUTPUT_START.innerHTML = "<form onsubmit='return false;'><label for='formName'>Money:</label><input type='text' id='moneyField' name='formMoney' placeholder='Money:'><input type='submit' onclick=getFormInput()></form>"
-    displayProduct("Chocolate bar", 4.5)
+    displayProduct("Chocolate bar", 2.5)
+    displayProduct("Cookie", 5)
 }
 function getFormInput(){
     const MONEY_FIELD = document.getElementById("moneyField");
     let userMoney = Number(MONEY_FIELD.value);
-    OUTPUT.innerHTML = "<p>money:"+userMoney+"</p>"
-    if (userMoney >= 4.5){
-        OUTPUT.innerHTML += "<p>you CAN buy a chocolate bar</p>"
-    } else {
-        OUTPUT.innerHTML += "<p>you CAN'T buy a chocolate bar</p>"
-    }
+    OUTPUT.innerHTML = "<p>money: "+userMoney+"</p>"
+    
 }
 function displayProduct(_name, _price){
-    OUTPUT.innerHTML += "<p>" + _name + ": $" + _price + "</p>";
+    OUTPUT_ITEMS.innerHTML += "<p>" + _name + ": $" + _price + "</p>";
+}
+function canBuyItem(_name, _price){
+    if (_price >= 4.5){
+        OUTPUT.innerHTML += "<p>you CAN buy a "+_name+"</p>"
+    } else {
+        OUTPUT.innerHTML += "<p>you CAN'T buy a "+_name+" bar</p>"
+    }
 }
