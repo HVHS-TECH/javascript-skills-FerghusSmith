1d3db46f4e494c531e44562743d754b37762d07c
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-13 00:22:39 +0000

    May 13, 2026, 12:22 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -11,16 +11,16 @@ Main Code
 const OUTPUT_START = document.getElementById("spaceForJavaScriptOutputStart");
 const OUTPUT_ITEMS = document.getElementById("spaceForJavaScriptOutputStart");
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+let likesArray = ["you hate chocolate", "You don't mind chocolate", "you like chocolate", "You love chocolte"];
 /*
 Functions
 */
 function start(){
     OUTPUT_START.innerHTML = "<form onsubmit='return false;'><label for='formName'>Rate chocolate from 0 to 3</label><input type='text' id='likesField' name='formLikes' placeholder='0-3'><input type='submit' onclick=getFormInput()></form>"
-    let chocolateLikes = ["you hate chocolate", "You don't mind chocolate", "you like chocolate", "You love chocolte"];
 }
 function getFormInput(){
     const LIKE_FIELD = document.getElementById("likesField");
     userLikes = Number(LIKE_FIELD.value);
     console.log("userLikes = "+userLikes+" (in getFormInput)")
-    console.log("user choice is "+chocolateLikes[userLikes])
+    console.log("user choice is "+likesArray[userLikes])
 }
