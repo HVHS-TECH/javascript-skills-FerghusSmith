08dd6cbedf1c63617ac76325cce871818363cb06
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-13 00:26:10 +0000

    May 13, 2026, 12:26 AM

--- t14_arrays_1.js
+++ t14_arrays_1.js
@@ -16,11 +16,12 @@ let likesArray = ["you hate chocolate", "You don't mind chocolate", "you like ch
 Functions
 */
 function start(){
-    OUTPUT_START.innerHTML = "<form onsubmit='return false;'><label for='formName'>Rate chocolate from 0 to 3</label><input type='text' id='likesField' name='formLikes' placeholder='0-3'><input type='submit' onclick=getFormInput()></form>"
+    OUTPUT_START.innerHTML = "<form onsubmit='return false;'><label for='formName'>Rate chocolate from 0-3<br></label><input type='text' id='likesField' name='formLikes' placeholder='0-3'><input type='submit' onclick=getFormInput()></form>"
 }
 function getFormInput(){
     const LIKE_FIELD = document.getElementById("likesField");
     userLikes = Number(LIKE_FIELD.value);
     console.log("userLikes = "+userLikes+" (in getFormInput)")
     console.log("user choice is "+likesArray[userLikes])
+    OUTPUT.innerHTML += likesArray[userLikes]+"<br>";
 }
