8827b6836d4a789bb9a85a9f1821a3d4537c36b8
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-14 01:50:29 +0000

    May 14, 2026, 1:50 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -10,15 +10,16 @@ Main Code
 */
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-let shoppingArray = ["Milk", "Mince", "Carrot", "Fizzy drink"];
+let shoppingArray = [];
 /*
 Functions 
 */
 function start(){
-    OUTPUT.innerHTML = "<form onsubmit='return false;'><label for='formName'>possible outcomes: Mince, Fizzy drink, Carrot and Milk<br></label><input type='number' id='shoppingField' name='formshopping' placeholder='0-3'><input type='submit' onclick=getFormInput()></form>"
+    OUTPUT.innerHTML = "<form onsubmit='return false;'><label for='formName'>possible outcomes: Mince, Fizzy drink, Carrot and Milk<br></label><input type='text' id='shoppingField' name='formshopping' placeholder='0-3'><input type='submit' onclick=getFormInput()></form>"
 }
 function getFormInput(){
     const SHOPPING_FIELD = document.getElementById("shoppingField");
-    userShops = Number(SHOPPING_FIELD.value);
-    OUTPUT.innerHTML = shoppingArray[userShops]+"<br>";
+    userShops = (SHOPPING_FIELD.value);
+    shoppingArray.push(userShops)
+    OUTPUT.innerHTML += shoppingArray[0];
 }
