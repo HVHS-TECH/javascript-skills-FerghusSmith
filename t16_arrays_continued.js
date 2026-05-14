05d2ce88c956a0c7c4b13c25773afb61632c91e5
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-14 02:11:06 +0000

    V10F array continued

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -13,7 +13,7 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 const LIST_OUTPUT = document.getElementById("listOutput");
 let shoppingArray = [];
 /*
-Functions 
+Functions
 */
 function start(){
     OUTPUT.innerHTML = "<form onsubmit='return false;'><label for='formName'>Add items to your shopping list:<br></label><input type='text' id='shoppingField' name='formshopping' placeholder='item'><input type='submit' onclick=getFormInput()></form>"
