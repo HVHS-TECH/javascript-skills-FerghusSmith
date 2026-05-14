3919fb5de160a1ab9ffa67457259e8795d46edd9
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-14 01:02:15 +0000

    May 14, 2026, 1:02 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -15,8 +15,10 @@ const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 Functions
 */
 function start(){
+    OUTPUT.innerHTML = "<br>"
+    console.log("Cleared start button pre for-loop in function 'start'")
     console.log("starting for-loop in function 'start'")
     for(let i=99; i=0; i--){
-        OUTPUT += "<p>"+i+" bottles of milk on the wall. "+i+" bottles of milk. Take 1 down, pass it around, "+(i-1)+" bottles of milk on the wall</p><br>"
+        OUTPUT.innerHTML += "<p>"+i+" bottles of milk on the wall. "+i+" bottles of milk. Take 1 down, pass it around, "+(i-1)+" bottles of milk on the wall</p><br>"
     }
 }
