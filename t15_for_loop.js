097036337cd5079e561c18acea37ec01671916f3
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-14 00:44:28 +0000

    May 14, 2026, 12:44 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -1,1 +1,27 @@
-/* Paste your code from the last task */
+/*
+Name of task: functions with parameters
+*/
+console.log("Running t07_functions_parameters.js");
+
+//Variables
+
+/*
+Main Code
+*/
+const OUTPUT_START = document.getElementById("spaceForJavaScriptOutputStart");
+const OUTPUT_ITEMS = document.getElementById("spaceForJavaScriptOutputStart");
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+let likesArray = ["you hate chocolate", "You don't mind chocolate", "you like chocolate", "You love chocolte"];
+/*
+Functions
+*/
+function start(){
+    OUTPUT_START.innerHTML = "<form onsubmit='return false;'><label for='formName'>Rate chocolate from 0-3<br></label><input type='text' id='likesField' name='formLikes' placeholder='0-3'><input type='submit' onclick=getFormInput()></form>"
+}
+function getFormInput(){
+    const LIKE_FIELD = document.getElementById("likesField");
+    userLikes = Number(LIKE_FIELD.value);
+    console.log("userLikes = "+userLikes+" (in getFormInput)")
+    console.log("user choice is "+likesArray[userLikes])
+    OUTPUT.innerHTML += likesArray[userLikes]+"<br>";
+}
