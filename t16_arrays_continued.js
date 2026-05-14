9f31a471d8974c726e941e8ec52b2a6f0a74ffe3
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-14 02:05:27 +0000

    May 14, 2026, 2:05 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -16,12 +16,12 @@ let shoppingArray = [];
 Functions 
 */
 function start(){
-    OUTPUT.innerHTML = "<form onsubmit='return false;'><label for='formName'>possible outcomes: Mince, Fizzy drink, Carrot and Milk<br></label><input type='text' id='shoppingField' name='formshopping' placeholder='0-3'><input type='submit' onclick=getFormInput()></form>"
+    OUTPUT.innerHTML = "<form onsubmit='return false;'><label for='formName'>Add items to your shopping list:<br></label><input type='text' id='shoppingField' name='formshopping' placeholder='item'><input type='submit' onclick=getFormInput()></form>"
 }
 function getFormInput(){
     const SHOPPING_FIELD = document.getElementById("shoppingField");
     userShops = (SHOPPING_FIELD.value);
     shoppingArray.push(userShops);
     console.log("hi now");
-    LIST_OUTPUT.innerHTML = "<p>You have added "+shoppingArray[shoppingArray.length-1]+" to the list" + LIST_OUTPUT.innerHTML;
+    LIST_OUTPUT.innerHTML = "<p>You have added '"+shoppingArray[shoppingArray.length-1]+"' to the list" + LIST_OUTPUT.innerHTML;
 }
