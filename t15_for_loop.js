c77c4578b78cce62fecf62718067ba808479af05
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-14 00:59:04 +0000

    May 14, 2026, 12:59 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -8,20 +8,15 @@ console.log("Running t07_functions_parameters.js");
 /*
 Main Code
 */
-const OUTPUT_START = document.getElementById("spaceForJavaScriptOutputStart");
-const OUTPUT_ITEMS = document.getElementById("spaceForJavaScriptOutputStart");
+
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
-let likesArray = ["you hate chocolate", "You don't mind chocolate", "you like chocolate", "You love chocolte"];
+
 /*
 Functions
 */
 function start(){
-    OUTPUT_START.innerHTML = "<form onsubmit='return false;'><label for='formName'>Rate chocolate from 0-3<br></label><input type='text' id='likesField' name='formLikes' placeholder='0-3'><input type='submit' onclick=getFormInput()></form>"
-}
-function getFormInput(){
-    const LIKE_FIELD = document.getElementById("likesField");
-    userLikes = Number(LIKE_FIELD.value);
-    console.log("userLikes = "+userLikes+" (in getFormInput)")
-    console.log("user choice is "+likesArray[userLikes])
-    OUTPUT.innerHTML += likesArray[userLikes]+"<br>";
+    console.log("starting for-loop in function 'start'")
+    for(let i=99; i=0; i--){
+        OUTPUT += "<p>"+i+" bottles of milk on the wall. "+i+" bottles of milk. Take 1 down, pass it around, "+(i-1)+" bottles of milk on the wall</p><br>"
+    }
 }
