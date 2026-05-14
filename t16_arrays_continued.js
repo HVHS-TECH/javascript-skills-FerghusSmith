0bfc2ed2a93779dc002613adb566bcf6419078d2
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-14 01:26:31 +0000

    May 14, 2026, 1:26 AM

--- t16_arrays_continued.js
+++ t16_arrays_continued.js
@@ -1,1 +1,26 @@
-/* Paste your code from the last task */
+/*
+Name of task: functions with parameters
+*/
+console.log("Running t16_arrays_continued.js");
+
+//Variables
+
+/*
+Main Code
+*/
+
+const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+/*
+Functions
+*/
+function start(){
+    OUTPUT_START.innerHTML = "<form onsubmit='return false;'><label for='formName'>shopping list:<br></label><input type='text' id='shoppingField' name='formshopping' placeholder='0-3'><input type='submit' onclick=getFormInput()></form>"
+}
+function getFormInput(){
+    const LIKE_FIELD = document.getElementById("likesField");
+    userLikes = Number(LIKE_FIELD.value);
+    console.log("userLikes = "+userLikes+" (in getFormInput)")
+    console.log("user choice is "+likesArray[userLikes])
+    OUTPUT.innerHTML = likesArray[userLikes]+"<br>";
+}
