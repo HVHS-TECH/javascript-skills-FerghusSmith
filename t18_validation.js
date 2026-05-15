6cebe1e7f649ea4da5c26a78d6acbccad1d8a3d6
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-15 02:55:48 +0000

    May 15, 2026, 2:55 AM

--- t18_validation.js
+++ t18_validation.js
@@ -11,7 +11,7 @@ Main Code
 
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 const LIST_OUTPUT = document.getElementById("listOutput");
-let shoppingArray = [];
+let numberArray = [];
 /*
 Functions
 */
@@ -19,9 +19,10 @@ function start(){
     OUTPUT.innerHTML = "<form onsubmit='return false;'><label for='formName'>pick a nuumber:<br></label><input type='number' id='numberField' name='formNumber' placeholder='1-10' min='0' max='10'><input type='submit' onclick=getFormInput()></form>"
 }
 function getFormInput(){
-    const SHOPPING_FIELD = document.getElementById("shoppingField");
-    userShops = (SHOPPING_FIELD.value);
-    shoppingArray.push(userShops);
-    console.log("hi now");
-    LIST_OUTPUT.innerHTML = "<p>You have added '"+shoppingArray[shoppingArray.length-1]+"' to the list" + LIST_OUTPUT.innerHTML;
+    const NUMBER_FIELD = document.getElementById("numberField");
+    userNumber = Number(NUMBER_FIELD.value);
+    console.log("userNumber = "+userNumber)
+    if(userNumber < 1){
+
+    }
 }
