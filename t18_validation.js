96306c863be2873a9e3c9ad25dd7d081b9de6128
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-18 21:09:39 +0000

    May 18, 2026, 9:09 PM

--- t18_validation.js
+++ t18_validation.js
@@ -26,6 +26,6 @@ function getFormInput(){
         console.log("userNumber is out of range");
     } else {
         console.log("userNumber = "+userNumber);
-        OUTPUT.innerHTML = "<p>Your number is "+userNumber+"</p>";
+        LIST_OUTPUT.innerHTML = "<p>Your number is "+userNumber+"</p>";
     }
 }
