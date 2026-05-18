c23a1486dc1448d9691eef33de593c11417df706
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-18 21:19:43 +0000

    May 18, 2026, 9:19 PM

--- t19_string_handling.js
+++ t19_string_handling.js
@@ -10,21 +10,15 @@ Main Code
 */
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 const LIST_OUTPUT = document.getElementById("listOutput");
-let numberArray = [];
+let nameArray = [];
 /*
 Functions
 */
 function start(){
-    OUTPUT.innerHTML = "<form onsubmit='return false;'><label for='formName'>pick a nuumber:<br></label><input type='number' id='numberField' name='formNumber' placeholder='1-10' min='0' max='10'><input type='submit' onclick=getFormInput()></form>"
+    OUTPUT.innerHTML = "<form onsubmit='return false;'><label for='formName'>Name:<br></label><input type='text' id='nameField' name='formName' placeholder='name'><input type='submit' onclick=getFormInput()></form>"
 }
 function getFormInput(){
-    const NUMBER_FIELD = document.getElementById("numberField");
-    userNumber = Number(NUMBER_FIELD.value);
+    const NAME_FIELD = document.getElementById("nameField");
+    userName = (NAME_FIELD.value);
     
-    if(userNumber < 1 || userNumber > 10){
-        console.log("userNumber is out of range");
-    } else {
-        console.log("userNumber = "+userNumber);
-        LIST_OUTPUT.innerHTML = "<p>Your number is "+userNumber+"</p>";
-    }
 }
