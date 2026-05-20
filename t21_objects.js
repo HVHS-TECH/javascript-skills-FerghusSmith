af904cd1070c3c3eabbac5bba0f9e1d77f586e00
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-20 00:23:57 +0000

    May 20, 2026, 12:23 AM

--- t21_objects.js
+++ t21_objects.js
@@ -10,28 +10,65 @@ Main Code
 */
 const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 const LIST_OUTPUT = document.getElementById("listOutput");
-const MINIMUM_LENGTH = 3;
+const MINIMUM_NAME_LENGTH = 3;
+const MINIMUM_NUMBER_LENGTH = 1;
 var validName;
+var users = [];
 /*
 Functions
 */
 function start(){
-    OUTPUT.innerHTML = "<form onsubmit='return false;'><label for='formName'>Name:<br></label><input type='text' id='nameField' name='formName' placeholder='name'><input type='submit' onclick=getFormInput()></form>"
+    OUTPUT.innerHTML = "<form onsubmit='return false;'><label for='formName'>Name:<br></label><input type='text' id='nameField' name='formName' placeholder='name'><br><label for='formAge'>Age:<br></label><input type='text' id='ageField' name='formAge' placeholder='age'><br><label for='formMoney'>Money:<br></label><input type='text' id='moneyField' name='formMoney' placeholder='money'><br><input type='submit' onclick=getFormInput()></form>"
 }
 function getFormInput(){
     console.log("running function 'getFormInput'")
     const NAME_FIELD = document.getElementById("nameField");
+    const AGE_FIELD = document.getElementById("ageField");
+    const MONEY_FIELD = document.getElementById("moneyField");
+    //pulling inputs into variables
     userName = (NAME_FIELD.value);
+    userAge = Number(AGE_FIELD.value);
+    userMoney = Number(MONEY_FIELD.value);
+    //validating variables
     validName = isNaN(userName);
+    validAge = isNaN(userAge);
+    validMoney = isNaN(userMoney);
     console.log("Valid name: "+validName);
-    if(validName == true && userName.length >= MINIMUM_LENGTH){
+    
+    if(validName == true && userName.length >= MINIMUM_NAME_LENGTH && validAge == false && validMoney == false){
         console.log("Name valid in if statement");
         LIST_OUTPUT.innerHTML = "<br><p>Your name is "+userName+"</p>";
-    } else if(validName != true){
-        console.log("Name invalid in if statement: name is a number");
-        LIST_OUTPUT.innerHTML = "<br><p>Name must not be a number</p>";
-    } else if(userName.length < MINIMUM_LENGTH){
-        console.log("Name invalid in if statement: name is too short (name length is: "+userName.length+")");
-        LIST_OUTPUT.innerHTML = "<br><p>Name must be at least three letters long</p>";
+        LIST_OUTPUT.innerHTML += "<p>You are "+userAge+" years old</p>";
+        LIST_OUTPUT.innerHTML += "<p>You have $"+userMoney+"</p>";
+    } else {
+        LIST_OUTPUT.innerHTML = "<br>"
+        findError(validName, userName, validAge, userAge, validMoney, userMoney)
+    }
+}
+
+function findError(_validName, _userName, _validAge, _userAge, _validMoney, _userMoney){
+    if(validName != true){
+        console.log("userName invalid in function 'findError'. statement: name is a number");
+        LIST_OUTPUT.innerHTML += "<br><p>Name must not be a number</p>";
+    }
+    if(userName.length < MINIMUM_NAME_LENGTH){
+        console.log("userName invalid in function 'findError'. statement: name is too short (name length is: "+userName.length+")");
+        LIST_OUTPUT.innerHTML += "<br><p>Name must be at least three letters long</p>";
+    }
+    if(validAge != false){
+        console.log("userAge invalid in function 'findError'. statement: age is a string");
+        LIST_OUTPUT.innerHTML += "<br><p>Age must be a number</p>";
+    }
+    if(userAge.length < MINIMUM_NUMBER_LENGTH){
+        console.log("userAge invalid in function 'findError'. statement: age is not filled out");
+        LIST_OUTPUT.innerHTML += "<br><p>Age must be filled out</p>";
+    }
+    if(validMoney != false){
+        console.log("userMoney invalid in function 'findError'. statement: money is a string");
+        LIST_OUTPUT.innerHTML += "<br><p>Money must be a number</p>";
+    }
+    if(userMoney.length < MINIMUM_NUMBER_LENGTH){
+        console.log("userMoney invalid in function 'findError'. statement: money is not filled out");
+        LIST_OUTPUT.innerHTML += "<br><p>Money must be filled out</p>";
     }
 }
