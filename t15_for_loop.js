9929cc35a76d908a7802418ecebad2bee5a077c0
FerghusSmith <24311fs@hvhs.school.nz>
2026-05-28 01:04:09 +0000

    May 28, 2026, 1:04 AM

--- t15_for_loop.js
+++ t15_for_loop.js
@@ -23,12 +23,12 @@ function start(){
     console.log("Cleared start button pre for-loop in function 'start'")
     //for-loop 99 bottles of water on the wall song 
     for (let i = 99; i > 2; i--) {
-        OUTPUT.innerHTML += "<p>"+i+" bottles of water on the wall. "+i+" bottles of water. Take 1 down, pass it around. "+(i-1)+" bottles of water on the wall.<p>";
+        OUTPUT.innerHTML += "<p>"+i+" bottles of dihydrogenmonoxide on the wall. "+i+" bottles of dihydrogenmonoxide. Take 1 down, pass it around. "+(i-1)+" bottles of water on the wall.<p>";
     } 
     if(i = 2){
-        OUTPUT.innerHTML += "<p>"+i+" bottles of water on the wall. "+i+" bottles of water. Take 1 down, pass it around. "+(i-1)+" bottle of water on the wall.<p>";
+        OUTPUT.innerHTML += "<p>"+i+" bottles of dihydrogenmonoxide on the wall. "+i+" bottles of dihydrogenmonoxide. Take 1 down, pass it around. "+(i-1)+" bottle of water on the wall.<p>";
     }
     if(i = 1){
-        OUTPUT.innerHTML += "<p>"+i+" bottle of water on the wall. "+i+" bottle of water. Take 1 down, pass it around. "+(i-1)+" bottles of water on the wall.<p>";
+        OUTPUT.innerHTML += "<p>"+i+" bottle of dihydrogenmonoxide on the wall. "+i+" bottle of dihydrogenmonoxide. Take 1 down, pass it around. "+(i-1)+" bottles of water on the wall.<p>";
     }
 }
